638dfa6c29d2c709c3363787be2357013de6a89b
Ian Serlin <ian@ianserlin.com>
2018-10-27 12:49:11 -0700

    first

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>One two three four, ship your code out the door.</h1>
+  </Layout>
+)
+
+export default IndexPage
